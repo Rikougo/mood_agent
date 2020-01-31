e2c3fbb3cfac20960656b4cccd033bfe6ed7fb58
Rikougo <rikougo.kagakusha@gmail.com>
2020-02-01 00:06:48 +0100

    Test hub, player movement

--- scripts/Samuel/main_menu.gd
+++ scripts/Samuel/main_menu.gd
@@ -1,7 +1,5 @@
 extends Node
 
-
-
 func _on_Play_pressed():
 	get_tree().change_scene("res://scenes/hub.tscn")
 
---
+++ scripts/Samuel/player_hub.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+const MAX_SPEED = 350
+const ACCELERATION = 20
+var motion = Vector2.ZERO
+
+func _ready():
+	pass
+	
+func _physics_process(delta):	
+	var axis = Vector2.ZERO
+	axis.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	axis.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	axis = axis.normalized()
+	
+	if axis.x != 0 || axis.y != 0:
+		motion = (motion + axis * ACCELERATION).clamped(MAX_SPEED)
+	else:
+		motion = motion.linear_interpolate(Vector2.ZERO, 0.1)
+	
+	move_and_slide(motion)
